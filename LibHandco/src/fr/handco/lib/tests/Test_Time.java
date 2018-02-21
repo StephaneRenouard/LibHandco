f76bb6dd1cf21beacf247013b08c72de12ddf9de
stephanerenouard <stephane.renouard@gmail.com>
2018-02-21 19:56:41 +0100

    Test_time class update

--- LibHandco/src/fr/handco/lib/tests/Test_Time.java
+++ LibHandco/src/fr/handco/lib/tests/Test_Time.java
@@ -10,19 +10,24 @@ public class Test_Time {
 	
 	public Test_Time() {
 		
+		System.out.println("starting");
 		long start_time = System.currentTimeMillis();
 	
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
-		int result = Time.get_sec_from_currentTimeMillis(start_time);
+		int sec = Time.get_sec_from_currentTimeMillis(start_time);
+		int min = Time.get_min_from_currentTimeMillis(start_time);
+		int hour = Time.get_hour_from_currentTimeMillis(start_time);
 		
-		System.out.println(result);
+		System.out.println(hour + " hour " + min + " min " + sec + " sec");
+		
+		
 	}
 
 }
