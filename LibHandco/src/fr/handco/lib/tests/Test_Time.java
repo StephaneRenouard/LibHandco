133c2f405c4e159cb050daf78c16aa106561007e
stephanerenouard <stephane.renouard@gmail.com>
2018-02-21 19:40:45 +0100

    update Test_Time

--- LibHandco/src/fr/handco/lib/tests/Test_Time.java
+++ LibHandco/src/fr/handco/lib/tests/Test_Time.java
@@ -1,10 +1,28 @@
 package fr.handco.lib.tests;
 
+import fr.handco.lib.time.Time;
+
 public class Test_Time {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Test_Time();
+	}
+	
+	public Test_Time() {
+		
+		long start_time = System.currentTimeMillis();
+	
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		int result = Time.get_sec_from_currentTimeMillis(start_time);
+		
+		System.out.println(result);
 	}
 
 }
